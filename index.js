708e1c28921f9a6ee9c95b3d2c8e41f3cee247b5
trmanj <miraconder28@gmail.com>
2026-03-29 10:02:37 +0300

    Refactor bot initialization and update messages

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const mineflayer = require('mineflayer');
 const http = require('http');
 
-// Render'ı uyanık tutan servis
 http.createServer((req, res) => {
-    res.write('Mirac Sistemi Aktif!');
+    res.write('Mirac Sistemi - Cift Bot Savasci!');
     res.end();
 }).listen(3000);
 
-// BOT 1: Mirac_Bot
-const bot1 = mineflayer.createBot({
-    host: 'Trmanj.aternos.me',
-    port: 59562,
-    username: 'Mirac_Bot',
-    version: '1.20.1' // Buraya sunucunun tam sürümünü yazarsan daha sağlam olur
-});
+// BOT 1: Klasik Mirac_Bot
+function startBot1() {
+    const bot1 = mineflayer.createBot({
+        host: 'Trmanj.aternos.me',
+        port: 59562,
+        username: 'Mirac_Bot',
+        version: false
+    });
+    bot1.on('spawn', () => console.log(">> Mirac_Bot ICERIDE!"));
+    bot1.on('end', () => setTimeout(startBot1, 15000));
+}
 
-bot1.on('spawn', () => console.log(">> [OK] Mirac_Bot girdi!"));
-bot1.on('end', () => setTimeout(() => process.exit(), 5000)); // Hata verirse servisi restart atar
-
-// BOT 2: Mirac_Olumsuz (30 saniye sonra)
-setTimeout(() => {
+// BOT 2: Mirac_Olumsuz (İsmini karışıklık olmasın diye koda sabitledik)
+function startBot2() {
     const bot2 = mineflayer.createBot({
         host: 'Trmanj.aternos.me',
         port: 59562,
-        username: 'Mirac_Olumsuz',
-        version: '1.20.1'
+        username: 'Mirac_Olumsuz', 
+        version: false
     });
-    bot2.on('spawn', () => console.log(">> [OK] Mirac_Olumsuz girdi!"));
-    bot2.on('end', () => setTimeout(() => process.exit(), 5000));
-}, 30000);
+    bot2.on('spawn', () => console.log(">> Mirac_Olumsuz ICERIDE!"));
+    bot2.on('end', () => setTimeout(startBot2, 15000));
+}
+
+// Operasyonu Başlat
+startBot1();
+setTimeout(startBot2, 45000); // 45 saniye sonra ikinciyi sok ki "another location" hatası vermesin
